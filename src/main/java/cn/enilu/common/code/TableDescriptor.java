f01f8b7c297e95ed8a8acac160dfb7c1eb150638
Wizzer <wizzer@qq.com>
2017-02-04 11:58:05 +0800

    fix: 修改下路径问题

--- src/main/java/cn/enilu/common/code/TableDescriptor.java
+++ src/main/java/cn/enilu/common/code/TableDescriptor.java
@@ -11,288 +11,293 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
  * 实体（表）基本信息描述类<br>
  * 作者: zhangtao <br>
  * 创建日期: 16-7-10<br>
  */
 public class TableDescriptor {
-	private final String basePackageName;
-	private final String baseUri;
-	private final List<ColumnDescriptor> columns = new ArrayList<ColumnDescriptor>();
-	
-	public final String name;//表名称
-	private String entityName;//实体类名称
-	private String pkType;//主键类型
-	private String comment;//注释
-	private String label="项";
-	private String serPackageName;//service包名
-	private String modPackageName;//models包名
+    private final String basePackageName;
+    private final String baseUri;
+    private final List<ColumnDescriptor> columns = new ArrayList<ColumnDescriptor>();
+
+    public final String name;//表名称
+    private String entityName;//实体类名称
+    private String pkType;//主键类型
+    private String comment;//注释
+    private String label = "项";
+    private String serPackageName;//service包名
+    private String modPackageName;//models包名
 
 
-	public TableDescriptor(String name, String entityName,String basePackageName, String baseUri,String serPackageName,String modPackageName) {
-		this.name = name;
-		this.entityName = entityName;
-		this.basePackageName = basePackageName;
-		this.serPackageName = serPackageName;
-		this.modPackageName = modPackageName;
-		if (!baseUri.endsWith("/")) {
-			baseUri = baseUri + "/";
-		}
-		this.baseUri = baseUri;
-	}
-	
-	public String getBaseUri() {
-		return baseUri;
-	}
+    public TableDescriptor(String name, String entityName, String basePackageName, String baseUri, String serPackageName, String modPackageName) {
+        this.name = name;
+        this.entityName = entityName;
+        this.basePackageName = basePackageName;
+        this.serPackageName = serPackageName;
+        this.modPackageName = modPackageName;
+        if (!baseUri.endsWith("/")) {
+            baseUri = baseUri + "/";
+        }
+        this.baseUri = baseUri;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getBaseUri() {
+        return baseUri;
+    }
 
-	public String getPlural() {
-		return English.plural(name);
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getPkType() {
-		return pkType;
-	}
+    public String getPlural() {
+        return English.plural(name);
+    }
 
-	public void setPkType(String pkType) {
-		this.pkType = pkType;
-	}
+    public String getPkType() {
+        return pkType;
+    }
 
-	public String getBasePackageName() {
-		return basePackageName;
-	}
+    public void setPkType(String pkType) {
+        this.pkType = pkType;
+    }
 
-	public List<ColumnDescriptor> getColumns() {
-		return columns;
-	}
+    public String getBasePackageName() {
+        return basePackageName;
+    }
 
-	public void addColumn(ColumnDescriptor column) {
-		columns.add(column);
-	}
+    public List<ColumnDescriptor> getColumns() {
+        return columns;
+    }
 
-	public String getClassName() {
-		return getEntityClassName();
-	}
+    public void addColumn(ColumnDescriptor column) {
+        columns.add(column);
+    }
 
-	public String getUriPrefix() {
-		if(getName().contains("_")){
-			return baseUri+getName().replace("_","/");
-		}
-		return baseUri + getName();
-	}
+    public String getClassName() {
+        return getEntityClassName();
+    }
 
-	public String getViewBasePath() {
-		return baseUri.replaceFirst("/", "") + getName().replace("_","/");
-	}
+    public String getUriPrefix() {
+        if (getName().contains("_")) {
+            return baseUri + getName().substring(getName().indexOf("_") + 1).replace("_", "/");
+        }
+        return baseUri + getName();
+    }
 
-	public String getModPackageName() {
-		return modPackageName;
-	}
+    public String getViewBasePath() {
+        if (getName().contains("_")) {
+            return baseUri.replaceFirst("/", "") + getName().substring(getName().indexOf("_") + 1).replace("_", "/");
+        }
+        return baseUri.replaceFirst("/", "") + getName().replace("_", "/");
+    }
 
-	public void setModPackageName(String modPackageName) {
-		this.modPackageName = modPackageName;
-	}
+    public String getModPackageName() {
+        return modPackageName;
+    }
 
-	public String getSerPackageName() {
-		return serPackageName;
-	}
+    public void setModPackageName(String modPackageName) {
+        this.modPackageName = modPackageName;
+    }
 
-	public void setSerPackageName(String serPackageName) {
-		this.serPackageName = serPackageName;
-	}
+    public String getSerPackageName() {
+        return serPackageName;
+    }
 
-	public String getEntityClassName() {
-		if(Strings.isBlank(entityName)){
-			return Utils.UPPER_CAMEL(name);
-		}
-		return entityName;
-	}
+    public void setSerPackageName(String serPackageName) {
+        this.serPackageName = serPackageName;
+    }
 
-	public String getEntityFullClassName() {
-		return basePackageName + "."+getModPackageName()+"." + getEntityClassName();
-	}
+    public String getEntityClassName() {
+        if (Strings.isBlank(entityName)) {
+            return Utils.UPPER_CAMEL(name);
+        }
+        return entityName;
+    }
 
-	public String getServiceFullClassName() {
-		return basePackageName + "."+getSerPackageName()+"." + getServiceClassName();
-	}
+    public String getEntityFullClassName() {
+        return basePackageName + "." + getModPackageName() + "." + getEntityClassName();
+    }
 
-	public String getEntityInstanceName() {
-		return Utils.LOWER_CAMEL(  name);
-	}
+    public String getServiceFullClassName() {
+        return basePackageName + "." + getSerPackageName() + "." + getServiceClassName();
+    }
 
-	public String getEntityInstancesName() {
-		return getEntityInstanceName() + "s";
-	}
+    public String getEntityInstanceName() {
+        return Utils.LOWER_CAMEL(name);
+    }
 
-	public String getServiceInstanceName() {
-		return getEntityInstanceName() + "Service";
-	}
+    public String getEntityInstancesName() {
+        return getEntityInstanceName() + "s";
+    }
 
-	public String getServiceClassName() {
-		return Utils.UPPER_CAMEL(getEntityClassName()) + "Service";
-	}
+    public String getServiceInstanceName() {
+        return getEntityInstanceName() + "Service";
+    }
 
-	public String getControllerClassName() {
-		return Utils.UPPER_CAMEL(getEntityClassName()) + "Controller";
-	}
+    public String getServiceClassName() {
+        return Utils.UPPER_CAMEL(getEntityClassName()) + "Service";
+    }
 
-	public void addPrimaryKeyColumn(String columnName) {
-		for (ColumnDescriptor column : columns) {
-			if (column.columnName.equals(columnName)) {
-				column.primary = true;
-				break;
-			}
-		}
-	}
-	
-	public ColumnDescriptor getPrimaryColumn() {
-		for (ColumnDescriptor column : columns) {
-			if (column.primary) {
-				return column;
-			}
-		}
-		return null;
-	}
-	
-	public String getPrimaryType() {
-		ColumnDescriptor columnDescriptor = getPrimaryColumn();
-		if (columnDescriptor == null) {
-			return null;
-		}
-		
-		return columnDescriptor.getSimpleJavaTypeName();
-	}
+    public String getControllerClassName() {
+        return Utils.UPPER_CAMEL(getEntityClassName()) + "Controller";
+    }
 
-	public String getTableAnnotation() {
-		return "@Table";
-	}
+    public void addPrimaryKeyColumn(String columnName) {
+        for (ColumnDescriptor column : columns) {
+            if (column.columnName.equals(columnName)) {
+                column.primary = true;
+                break;
+            }
+        }
+    }
 
-	public List<String> getImports() {
-		Set<String> klasses = new LinkedHashSet<String>();
+    public ColumnDescriptor getPrimaryColumn() {
+        for (ColumnDescriptor column : columns) {
+            if (column.primary) {
+                return column;
+            }
+        }
+        return null;
+    }
 
-		for (ColumnDescriptor column : columns) {
-			String klass = column.getJavaType();
-			if (klass.startsWith("java.lang") || klass.indexOf('.') == -1) {
-				continue;
-			}
-			klasses.add(column.getJavaType());
-		}
+    public String getPrimaryType() {
+        ColumnDescriptor columnDescriptor = getPrimaryColumn();
+        if (columnDescriptor == null) {
+            return null;
+        }
 
-		List<String> imports = new ArrayList<String>();
-		imports.addAll(klasses);
-		imports.add(null);
+        return columnDescriptor.getSimpleJavaTypeName();
+    }
 
-		imports.add(Serializable.class.getName());
-		imports.add(null);
+    public String getTableAnnotation() {
+        return "@Table";
+    }
 
-		klasses.clear();
-		if (klasses.size() > 0) {
-			imports.addAll(klasses);
-			imports.add(null);
-		}
+    public List<String> getImports() {
+        Set<String> klasses = new LinkedHashSet<String>();
 
-		return imports;
-	}
+        for (ColumnDescriptor column : columns) {
+            String klass = column.getJavaType();
+            if (klass.startsWith("java.lang") || klass.indexOf('.') == -1) {
+                continue;
+            }
+            klasses.add(column.getJavaType());
+        }
 
-	public List<ColumnDescriptor> getEnumColumns() {
-		List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
-		for (ColumnDescriptor column : columns) {
-			if (column.isEnum()) {
-				result.add(column);
-			}
-		}
-		return result;
-	}
-	//todo
-	public String getQueryColumns(String op) {
-		List<String> result =  new ArrayList<String>();
-		for (ColumnDescriptor column : columns) {
-			if (op.equals(column.getQueryOperator())) {
-				result.add("\"" + column.columnName + "\"");
-			}
-		}
+        List<String> imports = new ArrayList<String>();
+        imports.addAll(klasses);
+        imports.add(null);
 
-		if (result.isEmpty()) {
-			return null;
-		}
-		return "";
-	}
+        imports.add(Serializable.class.getName());
+        imports.add(null);
 
-	public List<ColumnDescriptor> getSearchableColumns() {
-		List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
+        klasses.clear();
+        if (klasses.size() > 0) {
+            imports.addAll(klasses);
+            imports.add(null);
+        }
 
-		for (ColumnDescriptor column : columns) {
-			if (!Strings.isBlank(column.getQueryOperator())) {
-				result.add(column);
-			}
-		}
+        return imports;
+    }
 
-		return result;
-	}
+    public List<ColumnDescriptor> getEnumColumns() {
+        List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
+        for (ColumnDescriptor column : columns) {
+            if (column.isEnum()) {
+                result.add(column);
+            }
+        }
+        return result;
+    }
 
-	public List<ColumnDescriptor> getLabeledColumns() {
-		List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
+    //todo
+    public String getQueryColumns(String op) {
+        List<String> result = new ArrayList<String>();
+        for (ColumnDescriptor column : columns) {
+            if (op.equals(column.getQueryOperator())) {
+                result.add("\"" + column.columnName + "\"");
+            }
+        }
 
-		for (ColumnDescriptor column : columns) {
-			if (column.getComment()!=null&&!column.primary) {
-				result.add(column);
-			}
-		}
-		return result;
-	}
-	
-	public String[] getLabeledColumnNames() {
-		List<ColumnDescriptor> columnDescriptors = getLabeledColumns();
-		List<String> result = new ArrayList<String>();
-		for(ColumnDescriptor descriptor:columnDescriptors){
-			result.add(descriptor.getFieldName());
-		}
-		return result.toArray(new String[0]);
+        if (result.isEmpty()) {
+            return null;
+        }
+        return "";
+    }
 
-	}
-	
-	public String getLabeledColumnNamesString() {
-		StringBuilder buf = new StringBuilder();
-		for (String name : getLabeledColumnNames()) {
-			buf.append("\"").append(name).append("\"").append(", ");
-		}
-		if (buf.length() > 2) {
-			buf.setLength(buf.length() - 2);
-		}
-		return buf.toString();
-	}
+    public List<ColumnDescriptor> getSearchableColumns() {
+        List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
 
-	public List<ColumnDescriptor> getIndexColumns() {
-		List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
+        for (ColumnDescriptor column : columns) {
+            if (!Strings.isBlank(column.getQueryOperator())) {
+                result.add(column);
+            }
+        }
 
-		for (ColumnDescriptor column : columns) {
-			if (!Strings.isBlank(column.getLabel())
-					&& !"id".equals(column.columnName)) {
-				result.add(column);
-			}
-		}
+        return result;
+    }
 
-		return result;
-	}
+    public List<ColumnDescriptor> getLabeledColumns() {
+        List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
 
-	public void setLabel(String label) {
-		this.label = label;
-	}
+        for (ColumnDescriptor column : columns) {
+            if (column.getComment() != null && !column.primary) {
+                result.add(column);
+            }
+        }
+        return result;
+    }
 
-	public String getLabel() {
-		return label;
-	}
+    public String[] getLabeledColumnNames() {
+        List<ColumnDescriptor> columnDescriptors = getLabeledColumns();
+        List<String> result = new ArrayList<String>();
+        for (ColumnDescriptor descriptor : columnDescriptors) {
+            result.add(descriptor.getFieldName());
+        }
+        return result.toArray(new String[0]);
 
-	public String getComment() {
-		return comment;
-	}
+    }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+    public String getLabeledColumnNamesString() {
+        StringBuilder buf = new StringBuilder();
+        for (String name : getLabeledColumnNames()) {
+            buf.append("\"").append(name).append("\"").append(", ");
+        }
+        if (buf.length() > 2) {
+            buf.setLength(buf.length() - 2);
+        }
+        return buf.toString();
+    }
+
+    public List<ColumnDescriptor> getIndexColumns() {
+        List<ColumnDescriptor> result = new ArrayList<ColumnDescriptor>();
+
+        for (ColumnDescriptor column : columns) {
+            if (!Strings.isBlank(column.getLabel())
+                    && !"id".equals(column.columnName)) {
+                result.add(column);
+            }
+        }
+
+        return result;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 
 }
